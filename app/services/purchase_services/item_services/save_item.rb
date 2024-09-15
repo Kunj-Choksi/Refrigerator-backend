8166e5b105d2088668bf7750f0927af8f634b4e7
Kunj-Choksi <kunj07soni@gmail.com>
2024-09-15 11:27:20 -0400

    Fixed Save Item Issue

--- app/services/purchase_services/item_services/save_item.rb
+++ app/services/purchase_services/item_services/save_item.rb
@@ -21,14 +21,13 @@ module PurchaseServices
       attr_reader :purchase_item, :params
 
       def assign_attributes
-        @purchase_item.assign_attributes(
-          name: params[:name],
-          price: params[:price],
-          quantity: params[:quantity],
-          unit: params[:unit],
-          expiration_date: params[:expiration_date],
-          has_no_expiry: params[:has_no_expiry]
-        )
+        @purchase_item.name = params[:name] if params[:name].present?
+        @purchase_item.price = params[:price] if params[:price].present?
+        @purchase_item.quantity = params[:quantity] if params[:quantity].present?
+        @purchase_item.unit = params[:unit] if params[:unit].present?
+        @purchase_item.expiration_date = params[:expiration_date] if params[:expiration_date].present?
+        @purchase_item.has_no_expiry = params[:has_no_expiry] if params[:has_no_expiry].present?
+        @purchase_item.used = params[:used] if params[:used].present?
       end
     end
   end
