5d96482c286e54f7e5cb0839d31613d6e7f9d0ee
Kunj-Choksi <kunj07soni@gmail.com>
2024-09-17 09:40:35 -0400

    Fixed date

--- app/sidekiq/app/notify_expiring_items_job.rb
+++ app/sidekiq/app/notify_expiring_items_job.rb
@@ -4,7 +4,7 @@ class App::NotifyExpiringItemsJob < ActiveJob::Base
   def perform(*_args)
     item = PurchaseServices::ItemServices::ExpiringItem.call
 
-    text = "#{item.name} is expiring on #{item.expiration_date}"
+    text = "#{item.name} is expiring on #{item.expiration_date.strftime('%B %e, %Y')}"
 
     FcmUtil::Fcm.call(text:, device_fcm_token: User::Device.first.fcm_token)
   end
